cc76466ad5167f359c8982e19f4b1cb1cd4245c0
Hiski Huhtanen <hhuhtane22@student.oulu.fi>
2025-04-21 22:50:12 +0300

    Shaders and textures

---
+++ 3D-AI-DEMO/Assets/Script/ShaderPosition.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ShaderPosition : MonoBehaviour
+{
+    public float radius = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("_Position", transform.position);
+        Shader.SetGlobalFloat("_Radius", radius);
+    }
+}
